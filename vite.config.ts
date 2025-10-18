e3af261e5b62ba34d252195f80911eb5b2195304
nexahelp <nexahelp@outlook.com>
2025-10-18 08:46:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
   build: {
-    sourcemap: false,
     rollupOptions: {
       onwarn(warning, warn) {
-        // Ignorar advertencias de tipos o importaciones que no detienen el build
         if (warning.code === "THIS_IS_UNDEFINED" || warning.code === "PLUGIN_WARNING") return;
         warn(warning);
       },
